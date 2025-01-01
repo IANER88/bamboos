982fa36a942e63a27b40a7817a049df038895004
L.Jay <bozhancom@163.com>
2025-01-01 23:03:03 +0800

    feat: add mount

--- src/hooks/use-reference.ts
+++ src/hooks/use-reference.ts
@@ -1,11 +1,9 @@
-import SignalReference from "@/signal/signal-reference";
-
 export type Reference = ReturnType<typeof useReference>
 
 const useReference = <S>(reference?: S) => {
-  const ref = new SignalReference(reference);
-
-  return ref;
+//  const ref = new SignalReference(reference);
+//
+//  return ref;
 }
 
 export {
